4581a2bc1cd585a475d3a9cc06b12bf61cf5a9a3
rezkus <rezha.kusuma@gmail.com>
2017-11-20 23:58:47 +0700

    added frontend

---
+++ dss.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html >
+
+<head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <title>Supermarket Inventory Procurement Prediction</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">
+                   <b>Supermarket Inventory Procurement Prediction [SIPP]</b><small> - A DSS/ES by Idham, Rezha, and Edvin</small>
+                </a>
+            </div>
+            <div class="col-md-1 col-md-offset-3">
+              <button id="es" class="btn btn-primary" style="margin-top: 7px">Expert System</button>
+              <!-- Change page to Expert System Page -->
+              <script type="text/javascript">
+                document.getElementById("es").onclick = function () {
+                location.href = "/dss/es.php";
+                };
+              </script>
+            </div>
+        </div>
+    </nav>
+
+
+
+    <br><br>
+    <div class="col-xs-6 col-centered panel panel-default">
+        <h3>Linear Programming</h3>
+      
+        <form method="post" action="main.php" name="linearRegression">
+            <div class="form-group">
+              <label for="exampleInputFile">File input</label>
+              <input type="file" id="regInputFile" name="LinearRegressionCSV">
+              <p class="help-block">File must be in .csv</p>
+            </div>
+            <button id="linearRegButton" type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+
+    <div class="col-xs-6 col-centered panel panel-default">
+        <h3>Logistic Programming</h3>
+
+        <form method="post" action="main.php" name="logisticRegression">
+            <div class="form-group">
+              <label for="exampleInputFile">File input</label>
+              <input type="file" id="exampleInputFile" name="LogisticRegressionCSV">
+              <p class="help-block">File must be in .csv</p>
+            </div>
+            <button id="logisticRegButton" type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+    
+</body>
+
+</html>
---
+++ es.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html >
+
+<head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <title>Supermarket Inventory Procurement Prediction</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">
+                   <b>Supermarket Inventory Procurement Prediction [SIPP]</b><small> - A DSS/ES by Idham, Rezha, and Edvin</small>
+                </a>
+            </div>
+            <div class="col-md-1 col-md-offset-2">
+              <button id="dss" class="btn btn-primary" style="margin-top: 7px">Decision Support System</button>
+              <!-- Change page to DSS System Page -->
+              <script type="text/javascript">
+                document.getElementById("dss").onclick = function () {
+                location.href = "/dss/dss.php";
+                };
+              </script>
+            </div>
+        </div>
+    </nav>
+
+    <br><br>
+    <div class="col-xs-6 col-centered panel panel-default">
+        <h3>Fill in the Expert System Query</h3>
+        <form method="post" action="esresult.php" name="expertSystem">
+            <div class="form-group">
+              <label for="month">Month</label>
+              <select class="form-control">
+                <option>January</option>
+                <option>February</option>
+                <option>March</option>
+                <option>April</option>
+                <option>May</option>
+                <option>June</option>
+                <option>July</option>
+                <option>August</option>
+                <option>September</option>
+                <option>October</option>
+                <option>November</option>
+                <option>December</option>
+              </select>
+            </div>
+
+            <div class="form-group">
+              <label for="category">Product Category</label>
+              <select class="form-control">
+                <option>Stationery</option>
+                <option>Fashion</option>
+                <option>Electronics</option>
+                <option>Cooking Utensils</option>
+              </select>
+            </div>
+
+            <button id="esButton" type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+
+    
+     
+</body>
+
+</html>
